90c526f65739d391af6e23b5d8f41c8fb53ff561
Florian VIVAUDOU <fvivaudo@e3r10p1.42.fr>
2015-05-02 14:23:34 +0200

    libft: add getnextline

--- libft/includes/libft.h
+++ libft/includes/libft.h
@@ -92,6 +92,7 @@ char			*ft_strsdup(const char *s1, size_t start);
 char			*ft_strcpy(char *s1, const char *s2);
 char			*ft_strncpy(char *s1, const char *s2, size_t n);
 char			*ft_strcat(char *s1, const char *s2);
+char			*ft_strcatf(const char *s1, char *s2);
 char			*ft_strncat(char *s1, const char *s2, size_t n);
 char			*ft_strchr(const char *s, int c);
 char			*ft_strrchr(const char *s, int c);
